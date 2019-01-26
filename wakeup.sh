e99fabb422d547065a7739301d159fbeaeecefe2
mkusaka <hinoshita1992@gmail.com>
2019-01-27 00:00:39 +0900

    ignore

--- wakeup.sh
+++ wakeup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 open http://0.0.0.0:8888
-docker run -it -p 8888:8888 -v ${PWD}/src:/root/sharedfolder --ipc=host mkusaka/deepo:all-jupyter-py36-cpu jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token= --notebook-dir='/root'
+docker run -it -p 8888:8888 -v ${PWD}/src:/root --ipc=host mkusaka/deepo:all-jupyter-py36-cpu jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token= --notebook-dir='/root'
